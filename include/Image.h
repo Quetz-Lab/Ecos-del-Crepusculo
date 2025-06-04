4471f955a6222ba5ef749bfcad82015a30e85173
Quetz-lab <quetz.yggdrasil24@outlook.com>
2025-06-04 01:41:33 -0600

    .

--- include/Image.h
+++ include/Image.h
@@ -1,11 +1,14 @@
-#pragma once
-#include "View.h"
-class Image :
-    public View, 
-{
-    Texture2D texture;
+//#pragma once
+//#include "View.h"
+//class Image :
+//    public View
+//{
+//	Texture2D texture;
+//	Image(const char* filepath, int x, int y, int w, int h);
+//	void draw() override;
+//public:
+//
+//	Image() = default;
+//};
 
-    Image(const char* filePath, int x, int y, int w, int h);
-    void draw() override;
-};
-
+ 
--- src/Image.cpp
+++ src/Image.cpp
@@ -1,13 +1,14 @@
 #include "Image.h"
 
-Image::Image(const char* filePath, int x, int y, int w, int h) : View(x,y,w,h) 
-{
-    texture = LoadTexture(filePath);
-}
-
-void Image::draw() {
-    if (visible) {
-        DrawTexture(texture, rect.x, rect.y, WHITE);
-        //DrawTextureEx(texture, rect.x, rect.y, WHITE);
-    }
-}
+//Image::Image(const char* filePath, int x, int y, int w, int h) : View (x, y, w, h) 
+//{
+//	texture = LoadTexture(filePath);
+//}
+//
+//void Image::draw()
+//{
+//	if (visible)
+//	{
+//		DrawTexture(texture, rect.x, rect.y, WHITE);
+//	}
+//}
